f6b2a11e8256fbd26dd6e014eff09790104328cf
Dmitry <shishkalov64@ya.ru>
2023-06-05 11:19:47 +0400

    Add

---
+++ src/pages/main/scripts.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        // type: "fraction",
+        type: 'custom',
+        renderCustom: function (swiper, current, total) {
+            return current + '<span class="swiper-separator"></span>' + total; 
+        }
+      },
+  });
